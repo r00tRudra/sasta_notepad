c9fe108f385ba6ee525f0ade8c8500f5568979cf
Rudra <bhowmickrudra07@gmail.com>
2026-05-21 11:40:19 +0530

    url set to vercel2

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 window.APP_CONFIG = window.APP_CONFIG || {};
-window.APP_CONFIG.API_BASE_URL = window.APP_CONFIG.API_BASE_URL || "https://sasta-notepad.vercel.app";
+window.APP_CONFIG.API_BASE_URL = window.APP_CONFIG.API_BASE_URL || "https://sasta-notepad-n6q1.vercel.app/";
